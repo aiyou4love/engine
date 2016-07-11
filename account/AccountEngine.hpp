f31f5b1ba6d974bc3ff206e391b2a55fff53a691
aiyou4love <aiyou4love@outlook.com>
2016-07-11 08:53:07 +0800

    u

--- account/AccountEngine.hpp
+++ account/AccountEngine.hpp
@@ -4,30 +4,14 @@ namespace cc {
 	
 	class AccountEngine : noncopyable
 	{
-	public:
-		void runReward(int32_t nRewardId, EntityPtr& nEntity, ValuePtr& nValue)
+	public:				
+		AccountEngine& instance();
 		
-	public:
-		template<class __t>
-		void headSerialize(__t& nSerialize)
-		{
-			nSerialize.runMapStreamPtrs<int32_t, RewardPtr>(mRewards, "rewards", "reward");
-		}
-		const char * streamName();
-		const char * streamUrl();
+		AccountEngine();
+		~AccountEngine();
 		
-		RewardEngine& instance();
-		
-		void runPreinit();
-		void runLoad();
-		
-		RewardEngine();
-		~RewardEngine();
-		
-	private:
-		map<int32_t, RewardPtr> mRewards;
-		
-		static RewardEngine mRewardEngine;
+	private:		
+		static AccountEngine mAccountEngine;
 	};
 	
 }
