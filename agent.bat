85723df474142224ae8344ee0849d9c1d7cddcd2
zyouhua <zyouhua@gmail.com>
2016-07-31 23:01:15 +0800

    u

---
+++ agent.bat
@@ -0,0 +1,11 @@
+@echo off
+if not exist client (
+	mkdir client
+)
+cd client
+::cmake .. -D__GAMEEXE__=client -G"Visual Studio 14 Win64"
+::pause
+cmake .. -D__GAMEEXE__=client -G"NMake Makefiles"
+nmake
+nmake install
+cd ..
